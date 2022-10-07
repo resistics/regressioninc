f374fabd594052a778e7d92d25af0f14e9a140a5
nss350 <nss350@gmail.com>
2022-10-07 12:31:05 +0100

    Add more methods to base LinearRegressor

--- regressioninc/base.py
+++ regressioninc/base.py
@@ -4,6 +4,8 @@ Base class for regression in C
 The base class implements some basic functions and the abstract classes for
 regressors.
 """
+from typing import Optional
+from pydantic import BaseModel
 import numpy as np
 
 
@@ -41,17 +43,64 @@ def add_intercept(X: np.ndarray) -> np.ndarray:
     return np.hstack((X, np.ones(shape=(n_samples, 1), dtype=X.dtype)))
 
 
-class Regressor:
+class Regressor(BaseModel):
     """Base class for any regressor"""
+
+    class Config:
+        arbitrary_types_allowed: bool = True
 
     pass
 
 
 class LinearRegressor(Regressor):
-    """Base class for a linear regressor"""
+    """Base class for LinearRegression"""
 
-    def fit(self):
+    coef: Optional[np.ndarray] = None
+    """The coefficients"""
+
+    def fit(self, X: np.ndarray, y: np.ndarray):
+        """Fit the linear model"""
         raise NotImplementedError("fit is not implemented in the base class")
 
-    def predict(self):
-        return NotImplementedError("predict is not implemented in the base class")
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Predict using the linear model
+
+        Parameters
+        ----------
+        X : np.ndarray
+            The predictors
+
+        Returns
+        -------
+        np.ndarray
+            The predicted observations
+
+        Raises
+        ------
+        ValueError
+            If the coefficients are None. This is most likely the case if the
+            model has not been fitted first.
+        """
+        if self.coef is None:
+            raise ValueError(f"{self.coef=}, fit the model first")
+        return np.matmul(X, self.coef)
+
+    def fit_predict(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        """
+        Fit and predict on predictors X and observations y
+
+        Parameters
+        ----------
+        X : np.ndarray
+            The predictors to use for the fit and predict
+        y : np.ndarray
+            The observations for the fit
+
+        Returns
+        -------
+        np.ndarray
+            The predicted observations
+        """
+        self.fit(X, y)
+        return self.predict(X)
